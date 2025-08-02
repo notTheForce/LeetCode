f0b3d2cbe513c0fce73d1578e06863e7caa43d5c
notTheForce <theforce.0904@gmail.com>
2025-08-02 16:00:13 +0530

    validParenthese

---
+++ arrays/validParentheses.py
@@ -0,0 +1,33 @@
+""""
+def isValid(s: str) -> bool:
+     for i in range(0,len(s)-1):
+        if s[i]=='(' and s[len(s)-i-1]==')' or s[i]=='{' and s[len(s)-i-1]=='}' or s[i]=='[' and s[len(s)-i-1]==']':
+            return True 
+        else:
+            return False
+        
+s="([{}])"
+print(isValid(s))
+
+Doesnt work for s=()[]{} or similar series 
+""""
+
+#Using stack
+def isValid(s: str)->bool:
+    stack=[]
+    closeToopen={ ")":"(", "]":"[", "}":"{" }
+    for char in s:
+        if char in closeToopen:
+            if stack and stack[-1]==closeToopen[char]:
+                return True
+            else:
+                return False
+        else:
+            stack.append(char)
+
+
+s="([{}])"
+s1='()[]'
+print(isValid(s))
+print(isValid(s1))
+
